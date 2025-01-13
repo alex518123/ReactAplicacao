758df100d8e8a58191599fe74cfbeece782b39f6
alex518123 <alexresende518@gmail.com>
2025-01-12 21:26:09 -0300

    Remove basename from HashRouter

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/ReactAplicacao">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
